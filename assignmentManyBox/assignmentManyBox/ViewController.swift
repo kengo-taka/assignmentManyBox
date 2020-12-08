d8d1b4a0955b224144e3ec8f97e92f4101741af3
kengo-taka <1117marine1117@gmail.com>
2020-12-08 13:43:53 -0800

    first commit

---
+++ assignmentManyBox/assignmentManyBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignmentManyBox
+//
+//  Created by Takamiya Kengo on 2020/12/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
